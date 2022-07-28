857f490653ca081e5d9217f701902b95e9a5022c
unknown <rohithmahadev30@gmail.com>
2022-07-28 18:41:35 +0530

    heads added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+
+st.title("Healthy Diner")
+
+st.header("Breakfast Menu")
+
+st.text("Omega 3 & Blueberry pancakes")
+st.text("Spinach and Rocket Smoothie")
+st.text("Hard Boiled Egg with Bagels ")
